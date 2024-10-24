bb7177f2bec06d5f500c87b302a9bc32e6de21de
Adam Rasfeld <arasfeld@gmail.com>
2024-10-24 16:16:11 -0400

    add react

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <div>
+    <h1>💖 Hello World!</h1>
+    <p>Welcome to your Electron application.</p>
+  </div>
+);
