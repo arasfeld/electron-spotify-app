63d5b04a174af0b844907ab8701a387ff8bb52e9
Adam Rasfeld <arasfeld@gmail.com>
2024-10-26 19:28:20 -0400

    add mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { createTheme, MantineProvider } from '@mantine/core';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
 
+const theme = createTheme({});
+
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  </BrowserRouter>
+  <MantineProvider theme={theme}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </MantineProvider>
 );
--- src/renderer.ts
+++ src/renderer.ts
@@ -26,7 +26,7 @@
  * ```
  */
 
-import './index.css';
+import '@mantine/core/styles.css';
 import './App';
 
 console.log('👋 This message is being logged by "renderer.ts", included via Vite');
