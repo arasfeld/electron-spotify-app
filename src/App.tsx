fcb33051d4500f7a20e01ebb50aff6f44fba49fc
Adam Rasfeld <arasfeld@gmail.com>
2024-10-31 12:06:13 -0400

    add redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import { createTheme, MantineProvider } from '@mantine/core';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
+import { store } from './store';
 
 const theme = createTheme({});
-
 const root = createRoot(document.getElementById('root'));
+
 root.render(
-  <MantineProvider theme={theme}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  </MantineProvider>
+  <Provider store={store}>
+    <MantineProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </MantineProvider>
+  </Provider>
 );
